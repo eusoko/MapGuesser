c63f05295ec26daf5fe7c3426519d93cd1f6cc85
Pőcze Bence <bence@pocze.ch>
2020-05-17 22:28:34 +0200

    MAPG-3 move index.php's view part to views/guess.php

--- views/guess.php
+++ views/guess.php
@@ -1,26 +1,10 @@
-<?php
-
-require '../main.php';
-
-// demo position
-$realPosition = new MapGuesser\Geo\Position(47.85239, 13.35101);
-
-// demo bounds
-$bounds = new MapGuesser\Geo\Bounds($realPosition);
-$bounds->extend(new MapGuesser\Geo\Position(48.07683,7.35758));
-$bounds->extend(new MapGuesser\Geo\Position(47.57496, 19.08077));
-
-?>
-
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="utf-8">
     <title>MapGuesser</title>
     <link rel="stylesheet" type="text/css" href="static/css/mapguesser.css">
 </head>
-
 <body>
     <div id="panorama"></div>
     <div id="guess">
@@ -36,5 +20,4 @@ $bounds->extend(new MapGuesser\Geo\Position(47.57496, 19.08077));
     <script src="static/js/mapguesser.js" async defer></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=<?= $_ENV['GOOGLE_MAPS_JS_API_KEY'] ?>&callback=initialize" async defer></script>
 </body>
-
 </html>
