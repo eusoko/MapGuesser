0b7012b4d25671e0bc72e28e56aefcb1fa10f0f1
Pőcze Bence <bence@pocze.ch>
2020-05-24 15:42:29 +0200

    MAPG-62 minify SVG files with html-minifier put minify calls into separate sh script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ ROOT_DIR=$(dirname $(readlink -f "$0"))/..
 . ${ROOT_DIR}/.env
 
 if [ -f ${ROOT_DIR}/installed ]; then
-    echo "Mapguesser is already installed! To force reinstall, delete file 'installed' from the root directory!"
+    echo "MapGuesser is already installed! To force reinstall, delete file 'installed' from the root directory!"
     exit 1
 fi
 
@@ -14,10 +14,9 @@ echo "Installing MapGuesser DB..."
 mysql --host=${DB_HOST} --user=${DB_USER} --password=${DB_PASSWORD} ${DB_NAME} < ${ROOT_DIR}/db/mapguesser.sql
 
 if [ -z "${DEV}" ] || [ "${DEV}" -eq "0" ]; then
-    echo "Uglifying JS and CSS files..."
+    echo "Minifying JS, CSS and SVG files..."
 
-    uglifyjs ${ROOT_DIR}/public/static/js/mapguesser.js -c -m -o ${ROOT_DIR}/public/static/js/mapguesser.js
-    cleancss ${ROOT_DIR}/public/static/css/mapguesser.css -o ${ROOT_DIR}/public/static/css/mapguesser.css
+    ${ROOT_DIR}/scripts/minify.sh
 fi
 
 touch ${ROOT_DIR}/installed
--- scripts/update.sh
+++ scripts/update.sh
@@ -5,8 +5,7 @@ ROOT_DIR=$(dirname $(readlink -f "$0"))/..
 . ${ROOT_DIR}/.env
 
 if [ -z "${DEV}" ] || [ "${DEV}" -eq "0" ]; then
-    echo "Uglifying JS and CSS files..."
+    echo "Minifying JS, CSS and SVG files..."
 
-    uglifyjs ${ROOT_DIR}/public/static/js/mapguesser.js -c -m -o ${ROOT_DIR}/public/static/js/mapguesser.js
-    cleancss ${ROOT_DIR}/public/static/css/mapguesser.css -o ${ROOT_DIR}/public/static/css/mapguesser.css
+    ${ROOT_DIR}/scripts/minify.sh
 fi
