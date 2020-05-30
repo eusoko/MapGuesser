2e2524474fca442c5d4757259b3e924dfb9b78e2
Pőcze Bence <bence@pocze.ch>
2020-05-30 23:16:42 +0200

    MAPG-81 add favicons

--- views/game.php
+++ views/game.php
@@ -6,6 +6,10 @@
     <title>MapGuesser</title>
     <link href="static/css/mapguesser.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500&family=Roboto+Mono:wght@300;500&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" sizes="192x192" href="static/img/favicon/192x192.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="static/img/favicon/96x96.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="static/img/favicon/32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="static/img/favicon/16x16.png">
 </head>
 <body>
     <div id="loading">
