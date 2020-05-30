c615f00d03996ba7008b7c3ba6796d05905273b0
Pőcze Bence <bence@pocze.ch>
2020-05-30 20:51:15 +0200

    MAPG-88 fix js file name in minifier

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -4,6 +4,6 @@ ROOT_DIR=$(dirname $(readlink -f "$0"))/..
 
 . ${ROOT_DIR}/.env
 
-uglifyjs ${ROOT_DIR}/public/static/js/mapguesser.js -c -m -o ${ROOT_DIR}/public/static/js/mapguesser.js
+uglifyjs ${ROOT_DIR}/public/static/js/game.js -c -m -o ${ROOT_DIR}/public/static/js/game.js
 cleancss ${ROOT_DIR}/public/static/css/mapguesser.css -o ${ROOT_DIR}/public/static/css/mapguesser.css
 svgo ${ROOT_DIR}/public/static/img/loading.svg -o ${ROOT_DIR}/public/static/img/loading.svg
