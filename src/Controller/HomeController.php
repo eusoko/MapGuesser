44db217e8b153c7a32f93706f70db62bd3e2bddc
Pőcze Bence <bence@pocze.ch>
2020-05-31 20:44:14 +0200

    MAPG-86 add home controller (only redirects now)

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,12 @@
+<?php namespace MapGuesser\Controller;
+
+use MapGuesser\Interfaces\Response\IRedirect;
+use MapGuesser\Response\Redirect;
+
+class HomeController
+{
+    public function getIndex(): IRedirect
+    {
+        return new Redirect([\Container::$routeCollection->getRoute('maps'), []], IRedirect::TEMPORARY);
+    }
+}
