a82b27cb909be9720346319ccb38eec20719b9dc
Pőcze Bence <bence@pocze.ch>
2020-06-01 20:42:37 +0200

    MAPG-97 add Leaflet

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,13 +9,14 @@ if [ -f ${ROOT_DIR}/installed ]; then
     exit 1
 fi
 
-echo "Installing MapGuesser DB..."
+echo "Installing Yarn packages..."
+(cd ${ROOT_DIR}/public/static && yarn install)
 
+echo "Installing MapGuesser DB..."
 mysql --host=${DB_HOST} --user=${DB_USER} --password=${DB_PASSWORD} ${DB_NAME} < ${ROOT_DIR}/db/mapguesser.sql
 
 if [ -z "${DEV}" ] || [ "${DEV}" -eq "0" ]; then
     echo "Minifying JS, CSS and SVG files..."
-
     ${ROOT_DIR}/scripts/minify.sh
 fi
 
--- scripts/update.sh
+++ scripts/update.sh
@@ -7,8 +7,10 @@ ROOT_DIR=$(dirname $(readlink -f "$0"))/..
 echo "Installing Composer packages..."
 (cd ${ROOT_DIR} && composer install)
 
+echo "Installing Yarn packages..."
+(cd ${ROOT_DIR}/public/static && yarn install)
+
 if [ -z "${DEV}" ] || [ "${DEV}" -eq "0" ]; then
     echo "Minifying JS, CSS and SVG files..."
-
     ${ROOT_DIR}/scripts/minify.sh
 fi
