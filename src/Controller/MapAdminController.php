dd2905286b478be5ca1af68e5eb0bed8fbfb88d0
Pőcze Bence <bence@pocze.ch>
2020-06-02 01:16:59 +0200

    MAPG-100 read and store panoId from/to database

--- src/Controller/MapAdminController.php
+++ src/Controller/MapAdminController.php
@@ -1,22 +1,31 @@
 <?php namespace MapGuesser\Controller;
 
 use MapGuesser\Database\Query\Select;
-use MapGuesser\Http\Request;
 use MapGuesser\Interfaces\Database\IResultSet;
 use MapGuesser\Interfaces\Response\IContent;
+use MapGuesser\Repository\PlaceRepository;
 use MapGuesser\Response\HtmlContent;
 use MapGuesser\Response\JsonContent;
 use MapGuesser\Util\Geo\Bounds;
 
 class MapAdminController
 {
-    public function getMaps(): IContent {
+    private PlaceRepository $placeRepository;
+
+    public function __construct()
+    {
+        $this->placeRepository = new PlaceRepository();
+    }
+
+    public function getMaps(): IContent
+    {
         //TODO
 
         return new HtmlContent('admin/maps');
     }
 
-    public function getMapEditor(array $parameters): IContent {
+    public function getMapEditor(array $parameters): IContent
+    {
         $mapId = (int) $parameters['mapId'];
 
         $bounds = $this->getMapBounds($mapId);
@@ -27,33 +36,13 @@ class MapAdminController
         return new HtmlContent('admin/map_editor', $data);
     }
 
-    public function getPlace(array $parameters) {
+    public function getPlace(array $parameters)
+    {
         $placeId = (int) $parameters['placeId'];
 
-        $select = new Select(\Container::$dbConnection, 'places');
-        $select->columns(['id', 'lat', 'lng']);
-        $select->whereId($placeId);
+        $placeData = $this->placeRepository->getById($placeId);
 
-        $place = $select->execute()->fetch(IResultSet::FETCH_ASSOC);
-
-        $request = new Request('https://maps.googleapis.com/maps/api/streetview/metadata', Request::HTTP_GET);
-        $request->setQuery([
-            'key' => $_ENV['GOOGLE_MAPS_SERVER_API_KEY'],
-            'location' => $place['lat'] . ',' . $place['lng'],
-            'source' => 'outdoor'
-        ]);
-
-        $response = $request->send();
-
-        $panoData = json_decode($response->getBody(), true);
-
-        if ($panoData['status'] !== 'OK') {
-            $panoId = null;
-        } else {
-            $panoId = $panoData['pano_id'];
-        }
-
-        $data = ['panoId' => $panoId];
+        $data = ['panoId' => $placeData['panoId']];
         return new JsonContent($data);
     }
 
@@ -73,12 +62,19 @@ class MapAdminController
     private function &getPlaces(int $mapId): array
     {
         $select = new Select(\Container::$dbConnection, 'places');
-        $select->columns(['id', 'lat', 'lng']);
+        $select->columns(['id', 'lat', 'lng', 'pano_id_cached', 'pano_id_cached_timestamp']);
         $select->where('map_id', '=', $mapId);
         $select->orderBy('lng');
-        //$select->limit(100);
 
-        $places = $select->execute()->fetchAll(IResultSet::FETCH_ASSOC);
+        $result = $select->execute();
+
+        $places = [];
+
+        while ($place = $result->fetch(IResultSet::FETCH_ASSOC)) {
+            $noPano = $place['pano_id_cached_timestamp'] && $place['pano_id_cached'] === null;
+
+            $places[] = ['id' => $place['id'], 'lat' => $place['lat'], 'lng' => $place['lng'], 'noPano' => $noPano];
+        }
 
         return $places;
     }
