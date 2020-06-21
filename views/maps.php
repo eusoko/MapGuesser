a2ca7888f4507be0091199cc697212775f420ebf
Pőcze Bence <bence@pocze.ch>
2020-06-21 17:36:08 +0200

    MAPG-160 replace space with margin-right next to inline images

--- views/maps.php
+++ views/maps.php
@@ -22,16 +22,16 @@ $cssFiles = [
                             <?php /* Copyright (c) 2019 The Bootstrap Authors. License can be found in 'USED_SOFTWARE' in section 'Bootstrap Icons'. */ ?>
                             <svg class="inline" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                            </svg>
-                            <?= $map['num_places'] ?> places
+                            </svg><!--
+                         --><?= $map['num_places'] ?> places
                         </p>
                         <p>
                             <?php /* Copyright (c) 2019 The Bootstrap Authors. License can be found in 'USED_SOFTWARE' in section 'Bootstrap Icons'. */ ?>
                             <svg class="inline" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" d="M12.5 2h-9V1h9v1zm-10 1.5v9h-1v-9h1zm11 9v-9h1v9h-1zM3.5 14h9v1h-9v-1z" />
                                 <path fill-rule="evenodd" d="M14 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 1a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 11a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 1a2 2 0 1 0 0-4 2 2 0 0 0 0 4zM2 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 1a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 11a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 1a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
-                            </svg>
-                            ~ <?= $map['area'][0] ?> <?= $map['area'][1] ?><sup>2</sup>
+                            </svg><!--
+                         -->~ <?= $map['area'][0] ?> <?= $map['area'][1] ?><sup>2</sup>
                         </p>
                     </div>
                     <p class="small justify marginTop"><?= $map['description'] ?></p>
--- views/templates/header.php
+++ views/templates/header.php
@@ -1,8 +1,8 @@
 <div class="header">
     <h1>
         <a href="/" title="MapGuesser">
-            <img class="inline" width="1em" height="1em" src="<?= $_ENV['STATIC_ROOT'] ?>/img/icon.svg?rev=<?= REVISION ?>">
-            <span>MapGuesser</span>
+            <img class="inline" width="1em" height="1em" src="<?= $_ENV['STATIC_ROOT'] ?>/img/icon.svg?rev=<?= REVISION ?>"><!--
+         --><span>MapGuesser</span>
         </a>
     </h1>
     <p class="header">
@@ -11,8 +11,8 @@
                 <?php /* Copyright (c) 2019 The Bootstrap Authors. License can be found in 'USED_SOFTWARE' in section 'Bootstrap Icons'. */ ?>
                 <svg class="inline" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                </svg>
-                <?= Container::$request->user()->getDisplayName() ?><!--
+                </svg><!--
+             --><?= Container::$request->user()->getDisplayName() ?><!--
             --></a></span><!--
             --><span><a href="/logout" title="Logout">Logout</a></span>
         <?php else : ?>
