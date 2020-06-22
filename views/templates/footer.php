675d7e60d03e529821fefd246190017d1fc85f6d
Pőcze Bence <bence@pocze.ch>
2020-06-22 21:17:02 +0200

    MAPG-165 don't write 'Release_' into footer

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,5 +1,5 @@
 </main>
 <footer>
-    <p><span class="bold"><?= $_ENV['APP_NAME'] ?></span> <?= VERSION ?></p><!--
+    <p><span class="bold"><?= $_ENV['APP_NAME'] ?></span> <?= str_replace('Release_', '', VERSION) ?></p><!--
  --><p>&copy; Pőcze Bence <?= (new DateTime(REVISION_DATE))->format('Y') ?></p>
 </footer>
