e7ee7bbe8e440d1b9aef48b7a907be7f15dae071
Pőcze Bence <bence@pocze.ch>
2020-06-25 14:01:17 +0200

    MAPG-178 add endpoint that can send valid data based on started session

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,12 +1,30 @@
 <?php namespace MapGuesser\Controller;
 
+use MapGuesser\Interfaces\Request\IRequest;
+use MapGuesser\Interfaces\Response\IContent;
 use MapGuesser\Interfaces\Response\IRedirect;
+use MapGuesser\Response\JsonContent;
 use MapGuesser\Response\Redirect;
 
 class HomeController
 {
+    private IRequest $request;
+
+    public function __construct(IRequest $request)
+    {
+        $this->request = $request;
+    }
+
     public function getIndex(): IRedirect
     {
         return new Redirect(\Container::$routeCollection->getRoute('maps')->generateLink(), IRedirect::TEMPORARY);
+    }
+
+    public function startSession(): IContent
+    {
+        // session starts with the request, this method just sends valid data to the client
+
+        $data = ['antiCsrfToken' => $this->request->session()->get('anti_csrf_token')];
+        return new JsonContent($data);
     }
 }
